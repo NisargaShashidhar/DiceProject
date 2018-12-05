b54ff9dd777bc6dcdaa7cc12d4eaea923115d579
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-12-05 09:15:18 -0500

    Update DiceHolder.java
    Created method to addDice to the holder with a maximum of 6 dice in the holder.

--- DiceHolder.java
+++ DiceHolder.java
@@ -2,10 +2,19 @@ import java.util.*;
 
 public class DiceHolder{
 
-	private ArrayList<Die> holder;
+	private ArrayList<Dice> holder;
 
 	public DiceHolder(){
-		holder = new ArrayList<Die>();
+		holder = new ArrayList<Dice>();
+	}
+
+	public int addDie(Dice die){
+		if(holder.size() < 6){
+			holder.add(die);
+			return 1;
+		}
+		else
+			return 1;
 	}
 
 }
