da874f4f572a2bdad73a4dec018f515078c03ca4
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-12-07 09:25:12 -0500

    Update DiceRunner.java
    Fixed errors in tests

--- DiceRunner.java
+++ DiceRunner.java
@@ -6,23 +6,23 @@ public class DiceRunner{
 		int tries = 0;
 		//Test 1
 		do{
+			tries++;
 			one.roll();
-			System.out.println("Dice 1 " +one);
+			System.out.println("Die 1 " +one);
 			two.roll();
-			System.out.println("Dice 2 "+ two);
+			System.out.println("Die 2 "+ two);
 			if(one.getValue() == 1 && two.getValue() == 1){
 				snakeEyes = true;
 				break;
 			}
-			tries++;
 			System.out.println();
 		}while(snakeEyes == false);
 		System.out.println("\nIt took "+tries+" rolls for each die to get snake eyes");
 
+		//Test 2
 		DiceHolder cup = new DiceHolder();
 		for(int i = 0; i < 6; i++){
-			Dice die = new Dice((int)(Math.random()*20)+1);
-			cup.addDie(die);
+			cup.addDie(new Dice((int)(Math.random()*20)+1));
 			System.out.println(cup);
 		}
 
